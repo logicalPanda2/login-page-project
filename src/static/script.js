5909203119f8d719baec140cec79afacb1d6e098
Marcel <marcelrg.business@gmail.com>
2026-02-17 14:14:37 +0700

    Rework logic

--- src/static/script.js
+++ src/static/script.js
@@ -5,78 +5,69 @@ const usernameError = document.getElementById("usernameError");
 const passwordError = document.getElementById("passwordError");
 const formError = document.getElementById("formError");
 
-// user object in local database
-const user1 = {
-    username: "Bomby",
-    password: "Bumba123",
+const DB = [
+    {
+        "username": "user0",
+        "password": "testpw123",
+    },
+]
+
+const errorCodes = {
+    8: "Value cannot be empty",
+    9: "Value cannot contain spaces",
+    10: "Invalid credentials",
+    11: "User not registered",
 }
 
-// validate the form on submit
 loginForm.onsubmit = () => {
     clearErrors();
-    let username = usernameInput.value;
-    let password = passwordInput.value;
-    let isUsernameValid = validateValue(username, "username");
-    let isPasswordValid = validateValue(password, "password");
-    if(!isUsernameValid || !isPasswordValid) {
-        return false;
-    }
-    let doesUserExist = checkUser(username, password);
-    if(doesUserExist === true) {
-        return true;
-    } else {
-        let errorMessage = "User is not registered";
-        updateErrors(formError, errorMessage);
-        return false;
-    }
-}
 
-function validateValue(value, field) {
-    value = value.trim();
-    if(value === "") {
-        let errorMessage = "value cannot be empty";
-        checkErrorLocation(field, errorMessage);
-        return false;
-    } else if(value.includes(" ")) {
-        let errorMessage = "value cannot contain spaces";
-        checkErrorLocation(field, errorMessage);
-        return false;
-    } 
-    value = value.split("");
-    if(value.length <= 4) {
-        let errorMessage = "value must be longer than 4 characters";
-        checkErrorLocation(field, errorMessage);
-        return false;
-    } else if(value.length >= 20) {
-        let errorMessage = "value must be shorter than 20 characters";
-        checkErrorLocation(field, errorMessage);
+    const username = usernameInput.value;
+    const password = passwordInput.value;
+    const resUsername = validateValue(username);
+    const resPassword = validateValue(password);
+
+    if(resUsername in errorCodes || resPassword in errorCodes) {
+        usernameError.textContent = errorCodes[resUsername];
+        passwordError.textContent = errorCodes[resPassword];
         return false;
     }
+
+    const user = DB.find(user => user.username === username);
+
+    if(!user) {
+        formError.textContent = errorCodes[11]
+        return false;
+    }
+
+    const isUserValid = checkUser(username, password, user);
+
+    if(!isUserValid) {
+        formError.textContent = errorCodes[10]
+        return false;
+    }
+
     return true;
 }
 
-function checkUser(username, password) {
-    if(username === user1["username"] && password === user1["password"]) {
-        return true;
-    }
+function validateValue(value) {
+    const trimmed = value.trim();
+    if(trimmed === "" || !trimmed) return 8;
+    else if(trimmed.includes(" ")) return 9;
+    else return 0;
+}
+
+function checkUser(username, password, user) {
+    if(
+        user.username === username &&
+        user.password === password
+    ) return true;
+
     return false;
 }
 
-function checkErrorLocation(field, errorMessage) {
-    if(field === "username") {
-        updateErrors(usernameError, errorMessage);
-        usernameInput.style.border = "1px solid red";
-    } else if(field === "password") {
-        updateErrors(passwordError, errorMessage);
-        passwordInput.style.border = "1px solid red";
-    }
-}
-
-function updateErrors(errorField, errorMessage) {
-    errorField.innerHTML = `${errorMessage}`;
-}
-
 function clearErrors() {
-    document.querySelectorAll('.errorMessage').forEach(errorField => errorField.innerHTML = "");
-    document.querySelectorAll('.inputField').forEach(inputFeld => inputFeld.style.border = "none");
+    usernameError.textContent = null;
+    passwordError.textContent = null;
+    formError.textContent = null;
 }
